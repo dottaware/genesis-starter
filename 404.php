7bc7d827c2c2b31e2b1516f1a6d7e6a06d629053
Stefano Dotta <stefano@twoswisshikers.net>
2019-02-10 18:42:22 +0100

    updated 404 page

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 /**
  * Summit Theme 404 page.
  *
- * 
+ *
  *
  * @package  Summit Theme
  * @author   Stefano Dotta
@@ -13,13 +13,13 @@
 // Remove default loop.
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 
-add_action( 'genesis_loop', 'genesis_404' );
+add_action( 'genesis_loop', 'dottaware_404_loop' );
 /**
  * This function outputs a 404 "Not Found" error message.
  *
  * @since 1.6
  */
-function genesis_404() {
+function dottaware_404_loop() {
 
 	genesis_markup(
 		array(
@@ -32,32 +32,16 @@ function genesis_404() {
 		array(
 			'open'    => '<h1 %s>',
 			'close'   => '</h1>',
-			'content' => apply_filters( 'genesis_404_entry_title', __( 'Not found, error 404', 'genesis' ) ),
+			'content' => apply_filters( 'genesis_404_entry_title', __( 'Not found, error 404', 'dottaware' ) ),
 			'context' => 'entry-title',
 		)
 	);
 
 	echo '<div class="entry-content">';
 
-	if ( genesis_html5() ) :
-		/* translators: %s: URL for current website. */
-		echo apply_filters( 'genesis_404_entry_content', '<p>' . sprintf( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it by using the search form below.', 'genesis' ), trailingslashit( home_url() ) ) . '</p>' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		get_search_form();
-
-	else :
-		?>
-
-		<p><?php /* translators: %s: URL for current website. */ printf( wp_kses_post( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it with the information below.', 'genesis' ) ), esc_url( trailingslashit( home_url() ) ) ); ?></p>
-
-		<?php
-	endif;
-
-	if ( genesis_a11y( '404-page' ) ) {
-		echo '<h2>' . esc_html__( 'Sitemap', 'genesis' ) . '</h2>';
-		genesis_sitemap( 'h3' );
-	} else {
-		genesis_sitemap( 'h4' );
-	}
+	/* translators: %s: URL for current website. */
+	echo apply_filters( 'genesis_404_entry_content', '<p>' . sprintf( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it by using the search form below.', 'dottaware' ), trailingslashit( home_url() ) ) . '</p>' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	get_search_form();
 
 	echo '</div>';
 
@@ -69,5 +53,6 @@ function genesis_404() {
 	);
 
 }
+
 
 genesis();
